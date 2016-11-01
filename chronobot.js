a39dd7f9caf7b55e04132d542e8eb158f3b150f4
chronomeister <chronomeista@gmail.com>
2016-11-01 16:16:57 -0500

    added timeout

--- chronobot.js
+++ chronobot.js
@@ -6,6 +6,10 @@ var configfile = './cbotconfig.json';
 
 var configs = jsonfile.readFileSync(configfile);
 
+const TIMEOUT = 2 * 1000;
+
+var timeouts = {};
+
 var bot = new Discord.Client();
 
 bot.on("message", msg => {
@@ -17,11 +21,14 @@ bot.on("message", msg => {
     if (!msg.content.startsWith(prefix)) return;
     var command = msg.content.substr(1);
     if (command.startsWith("idolhell")) {
+        var lastrun = timeouts.idolhell || 0;
+        if (Date.now() - TIMEOUT < lastrun) {return;}
         command.substr(9);
         if (command.trim().length > 9) {
             msg.channel.sendMessage(msg.author + ", your " + command.trim().substr(9) + " idol is:");
         }
         idolgame.idolhell(msg);
+        timeouts.idolhell = Date.now();
     }
 });
 
