77e9434602936901ed70d6c90ec89b1b7df1b18c
chronomeister <chronomeista@gmail.com>
2016-11-04 10:24:46 -0500

    Basic idle support

---
+++ idlemas.js
@@ -0,0 +1,32 @@
+var exports = module.exports = {};
+
+// map of (users, timeleft)
+var idlemap = new Map();
+
+//
+
+var AFKCOUNTDOWN = 15;
+
+exports.CHECKINTERVAL = 5 * 1000;
+
+exports.updateIdle = function() {
+    idlemap.forEach(function(value,key,map){
+        var newval =  idlemap.get(key) - 1;
+        if (newval < 0) {
+            idlemap.delete(key);
+        } else {
+            console.dir(key + " : " + idlemap.get(key));
+            idlemap.set(key,newval);
+        }
+    });
+}
+
+exports.addUser = function(author) {
+    idlemap.set(author,AFKCOUNTDOWN);
+    // console.dir(author);
+}
+
+exports.getScore = function(msg) {
+    msg.channel.sendMessage(msg.author + " has " + idlemap.get(msg.author) + " points");
+    // console.dir(author);
+}
