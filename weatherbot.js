70359f9f9e46967eecd83c408b629543ee179ea4
chronomeister <chronomeista@gmail.com>
2016-11-11 11:54:34 -0600

    more work on Weather

--- weatherbot.js
+++ weatherbot.js
@@ -10,35 +10,60 @@ exports.getWeather = function(msg, params) {
     var cmd = params.shift();
     var useImp = (cmd === "I");
     cmd = useImp ? params.shift() : cmd;
-    if (cmd) { // parameters present
+    var disid;
+    console.dir(msg.author);
+    db.get("SELECT disid FROM weatherusers WHERE disid = ?", [msg.author.id], function(err, row){disid = row ? row.disid : undefined;});
+    if (cmd && disid) { // parameters and user present
         switch (cmd) {
-            // check if user exists
             case "register":
-                // if not add
+                // if not add/update
+                db.run("INSERT OR REPLACE INTO weatherusers(disid, location) VALUES (?,?)", [disid, params.join(" ")]);
+                msg.channel.sendMessage("Your location is now registered to " + params.join(" "));
+                // check if valid location??
                 break;
             case "useMetric":
+                if (disid) {
+                    db.run("UPDATE weatherusers SET usemetric = !usemetric WHERE disid =?", [disid]);
+                    db.get("SELECT usemetric FROM weatherusers WHERE disid = ?", [msg.author.id], function(err, row) {
+                        msg.channel.sendMessage("You are now using " + (row.useMetric ? "Metric" : "Imperial"));
+                    });
+                }
                 // change metric
                 break;
             case "dispLoc":
-                // toggle disploc
+                if (disid) {
+                    db.run("UPDATE weatherusers SET disploc = !disploc WHERE disid =?", [disid]);
+                    db.get("SELECT disploc FROM weatherusers WHERE disid = ?", [msg.author.id], function(err, row) {
+                        msg.channel.sendMessage("I will now " + (row.disploc ? "start" : "stop") + " showing your location on weather requests.");
+                    });
+                }
+                // change metric
                 break;
             default:
+                msg.channel.sendMessage("Could not find user/command. Please either check the command or register");
                 // get weather
         }
-    } else { // bare word. check if user present, if not return.
+    } else if (disid) { // bare word. check if user present, if not return.
+        var loc, disp;
+        db.get("SELECT location, disploc FROM weatherusers WHERE disid = ?", [msg.author.id], function(err, row){
+            loc = row ? row.location : undefined;
+            disp = row ? row.disploc : undefined;
+        });
+        msg.channel.sendMessage("Getting weather for: you");
+        var wuurl = "http://api.wunderground.com/api/6a53a8043eb7b05b/geolookup/conditions/forecast/q/61081.json"
 
+        request(wuurl, function (error, response, html) {
+            if (!error && response.statusCode == 200) {
+                var curweath = JSON.parse(response.body);
+                printWeather(msg, curweath);
+                console.dir(curweath);
+                // msg.channel.sendMessage(dbhost + idol.file_url);
+            }
+        });
+    } else { // tell he is not present
+        var loc;
+        db.get("SELECT location FROM weatherusers WHERE disid = ?", [msg.author.id], function(err, row){disid = row.disid;});
     }
-    msg.channel.sendMessage("Getting weather for: you");
-    var wuurl = "http://api.wunderground.com/api/6a53a8043eb7b05b/geolookup/conditions/forecast/q/61081.json"
-
-    request(wuurl, function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-            var curweath = JSON.parse(response.body);
-            printWeather(msg, curweath);
-            console.dir(curweath);
-            // msg.channel.sendMessage(dbhost + idol.file_url);
-        }
-    });
 }
 
 function printWeather(msg, weaobj) {
