d787e2817b53081143802031edd1c9490730658a
chronomeister <chronomeista@gmail.com>
2017-03-06 16:17:56 -0600

    FRIENDS (untested)

---
+++ friends.js
@@ -0,0 +1,21 @@
+var exports = module.exports = {};
+exports.time = function(msg, qary) {
+    // assume UTC for everything
+    var serval = msg.guild.emojis.find(function(emoji){return emoji.name === 'waaaisugoooi';});
+    var firstep = new Date(Date.UTC(2017,00,10,16,35));
+    var now = Date.now();
+    var nextepnum = Math.floor(Math.floor(Math.floor(Math.floor(Math.floor((time - firstep) / 1000) / 60) / 60) / 24) / 7) + 2;
+    var nexteptime = new Date(firstep.getTime() + (nextepnum - 1) * 7 * 24 * 60 * 60 * 1000);
+    var time = nexteptime - now
+    var days = Math.floor(Math.floor(Math.floor(Math.floor(time / 1000) / 60) / 60) / 24);
+    var hours = Math.floor(Math.floor(Math.floor(time / 1000) / 60) / 60) % 24;
+    var minutes = Math.floor(Math.floor(time / 1000) / 60) % 60;
+    var seconds = Math.floor(time / 1000) % 60;
+
+    var message = ` ${serval} ` + "Episode " + nextepnum + " airs in "
+        ((days > 0) ? days + " day" + (days == 1 ? "" : "s") + " " : "") +
+        ((hours > 0) ? hours + " hour" + (hours == 1 ? "" : "s") + " " : "") +
+        ((minutes > 0) ? minutes + " minute" + (minutes == 1 ? "" : "s") + " " : "") +
+        ((seconds > 0) ? seconds + " second" + (seconds == 1 ? "" : "s") + " " : "") +
+        ` ${serval}`;
+}
