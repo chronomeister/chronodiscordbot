dc5d8e1c268ae3f33458dfc0837a91072a1b2824
chronomeister <masay87@yahoo.com>
2017-05-02 20:44:45 -0500

    Abstracted the webhooks a bit

--- webhook.js
+++ webhook.js
@@ -10,11 +10,11 @@ var client = new Twitter({
   bearer_token: auth.twitterbtoken
 });
 
-var username = ["KanColle_STAFF","KCIconWatcher"];
+var whconfig = require('./webhookconfig.json');
 
 var lastseenid = {};
-username.forEach(function(user){
-    lastseenid[user] = "";
+whconfig.forEach(function(user){
+    user.lastseenid = "";
 });
 
 /**
@@ -22,33 +22,34 @@ username.forEach(function(user){
  * number of tweets per second depends on topic popularity
  **/
 function getStatus(first) {
-    Object.keys(lastseenid).forEach(function(user){
+    whconfig.forEach(function(user){
         // console.log(user);
         if (first) {
-            client.get('statuses/user_timeline', {screen_name : user}, function(error, tweets, response) {
+            client.get('statuses/user_timeline', {screen_name : user.screen_name}, function(error, tweets, response) {
                 // console.log(JSON.parse(response.body));
-                lastseenid[user] = JSON.parse(response.body)[0].id_str;
-                // console.dir(lastseenid);
+                user.lastseenid = JSON.parse(response.body)[0].id_str;
+                // console.dir(user);
             });
         } else {
-            client.get('statuses/user_timeline', {screen_name : user, since_id : lastseenid[user]}, function(error, tweets, response) {
+            client.get('statuses/user_timeline', {screen_name : user.screen_name, since_id : user.lastseenid}, function(error, tweets, response) {
                 // fs.writeFile('./twitter.txt', response.body, function(){}); return;
                 var tweets = JSON.parse(response.body);
                 var ids = tweets.map(function(status){
                     return status.id_str;
                 });
                 // console.log(user);
-                lastseenid[user] = ids[0] ? ids[0] : lastseenid[user];
+                user.lastseenid = ids[0] ? ids[0] : user.lastseenid;
                 while (ids.length > 0) {
-
                     var newtweetid = ids.pop();
-                    request.post({url:'https://discordapp.com/api/webhooks/304453640138260481/smTg_XBiNvPHzaSQk0TUOvhAMYpxFxa5L3JOxOTlrxeU4A71SJfxTeyYv7_Y0W-F2DWA',
-                    // request.post({url:'https://discordapp.com/api/webhooks/303917161221586957/eQp2qhFeqtWe3PsRe_qFdJpVD9_5oB2ZTJpYvgZmAMKEox1coVsU3JEuxwaeCXE56Kb4',
-                        form: {
-                            content:`https://twitter.com/${user}/status/${newtweetid}`
-                        }},
-                        function(err, rsp, body){}
-                    );
+                    // request.post({url:'https://discordapp.com/api/webhooks/304453640138260481/smTg_XBiNvPHzaSQk0TUOvhAMYpxFxa5L3JOxOTlrxeU4A71SJfxTeyYv7_Y0W-F2DWA',
+                    user.webhooks.forEach(function(url){
+                        request.post({url:url,
+                            form: {
+                                content:`https://twitter.com/${user.screen_name}/status/${newtweetid}`
+                            }},
+                            function(err, rsp, body){}
+                        );
+                    });
                 }
             });
         }
@@ -57,7 +58,7 @@ function getStatus(first) {
 getStatus(1);
 
 // setTimeout(getStatus, 1000);
-setInterval(getStatus, 5000);
+setInterval(getStatus, 1500 * 1000 * (whconfig.length + 1) / (15 * 60));
 
 // client.stream('site', {follow: '448311788'}, function(stream) {
 //   stream.on('data', function(event) {
