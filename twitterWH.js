5569568e7c9a15478b7455f523ffbe89aa88bd73
chronomeister <masay87@yahoo.com>
2017-05-02 20:07:13 -0500

    Tested twitter webhook

--- twitterWH.js
+++ twitterWH.js
@@ -1,8 +1,22 @@
 var request = require('request');
 
-request.post({url:'https://discordapp.com/api/webhooks/273124540417048577/M_P4HuQstYFYy_WFxWrgF83BUw1-q80LD52yEs2SfCZJCvmstc3SJLCaCO6Kw5eieRf0',
-    form: {
-        content:'Muri'
-    }},
-    function(err, rsp, body){console.dir(rsp);console.dir(err);console.dir(body);}
-);
+// request.post({url:'https://discordapp.com/api/webhooks/273124540417048577/M_P4HuQstYFYy_WFxWrgF83BUw1-q80LD52yEs2SfCZJCvmstc3SJLCaCO6Kw5eieRf0',
+//     form: {
+//         content:'Muri'
+//     }},
+//     function(err, rsp, body){console.dir(rsp);console.dir(err);console.dir(body);}
+// );
+//Install this module to send easily your requests
+var auth = require('./cbotconfig.json');
+
+var a = Buffer(auth.twitterkey + ':' + auth.twittersecret).toString('base64');
+request.post({
+    url : 'https://api.twitter.com/oauth2/token',
+    headers : {
+        'Authorization': 'Basic '+ a,
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    body : 'grant_type=client_credentials'
+}, function (error, response, html) {
+    console.log(JSON.parse(response.body).access_token);
+});
--- webhook.js
+++ webhook.js
@@ -22,28 +22,30 @@ username.forEach(function(user){
  * number of tweets per second depends on topic popularity
  **/
 function getStatus(first) {
-    username.forEach(function(user){
+    Object.keys(lastseenid).forEach(function(user){
+        // console.log(user);
         if (first) {
             client.get('statuses/user_timeline', {screen_name : user}, function(error, tweets, response) {
-                // console.log(JSON.parse(response.body)[0].id_str);
+                // console.log(JSON.parse(response.body));
                 lastseenid[user] = JSON.parse(response.body)[0].id_str;
                 // console.dir(lastseenid);
             });
         } else {
             client.get('statuses/user_timeline', {screen_name : user, since_id : lastseenid[user]}, function(error, tweets, response) {
-                fs.writeFile('./twitter.txt', response.body, function(){}); return;
+                // fs.writeFile('./twitter.txt', response.body, function(){}); return;
                 var tweets = JSON.parse(response.body);
                 var ids = tweets.map(function(status){
                     return status.id_str;
                 });
+                // console.log(user);
                 lastseenid[user] = ids[0] ? ids[0] : lastseenid[user];
                 while (ids.length > 0) {
-                    console.log(ids[0]);
-                    var newtweetid = newids.pop();
-                    // request.post({url:'https://discordapp.com/api/webhooks/304453640138260481/smTg_XBiNvPHzaSQk0TUOvhAMYpxFxa5L3JOxOTlrxeU4A71SJfxTeyYv7_Y0W-F2DWA',
-                    request.post({url:'https://discordapp.com/api/webhooks/303917161221586957/eQp2qhFeqtWe3PsRe_qFdJpVD9_5oB2ZTJpYvgZmAMKEox1coVsU3JEuxwaeCXE56Kb4',
+
+                    var newtweetid = ids.pop();
+                    request.post({url:'https://discordapp.com/api/webhooks/304453640138260481/smTg_XBiNvPHzaSQk0TUOvhAMYpxFxa5L3JOxOTlrxeU4A71SJfxTeyYv7_Y0W-F2DWA',
+                    // request.post({url:'https://discordapp.com/api/webhooks/303917161221586957/eQp2qhFeqtWe3PsRe_qFdJpVD9_5oB2ZTJpYvgZmAMKEox1coVsU3JEuxwaeCXE56Kb4',
                         form: {
-                            content:`http://twitter.com/${username}/status/${newtweetid}`
+                            content:`https://twitter.com/${user}/status/${newtweetid}`
                         }},
                         function(err, rsp, body){}
                     );
@@ -51,8 +53,6 @@ function getStatus(first) {
             });
         }
     });
-
-    return;
 }
 getStatus(1);
 
