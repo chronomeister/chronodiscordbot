0aba8696c98216d828435629e7deedadc4c38923
chronomeister <chronomeista@gmail.com>
2017-06-07 15:38:07 -0500

    fixed cx with new host

--- cx.js
+++ cx.js
@@ -7,21 +7,23 @@ exports.currencyexchange = function(msg, cx) {
     if (cx[1] && !/[\d.$¥]+/.test(cx[1])) {msg.channel.send("Invalid number. Please restrict your value to numbers and decimal point."); return;}
     var from = cx[0].substr(0,3);
     var to =  cx[0].substr(3,3);
-    var cxurl = `http://rate-exchange-1.appspot.com/currency?from=${from}&to=${to}`
+    var cxurl = `http://api.fixer.io/latest?base=${from}&symbols=${to}`
     // msg.reply(cxurl);
     request(cxurl, function (error, response, html) {
         if (!error && response.statusCode == 200) {
             var cxrate = JSON.parse(response.body);
-            // console.dir(response.body);
-            if (cxrate.err) {
+			//console.dir(response);
+            if (!cxrate.rates[to] || cxrate.error) {
                 msg.channel.send("Invalid country code(s)");
             } else if (cx[1]) {
-                msg.channel.send(cx[1] + " " + from + " in " + to + " is: " + (cxrate.rate * cx[1]));
+                msg.channel.send(cx[1] + " " + from + " in " + to + " is: " + (cxrate.rates[to] * cx[1]));
             } else {
-                msg.channel.send("Current " + from + " to " + to + " rate is: " + cxrate.rate);
+                msg.channel.send("Current " + from + " to " + to + " rate is: " + cxrate.rates[to]);
             }
         } else if (!error && response.statusCode == 500) {
             msg.channel.send("Internal server error. Try again later.");
+		} else if (!error && response.statusCode == 422) {
+            msg.channel.send("Invalid country code(s)");
         }
     });
 }
