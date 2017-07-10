6816b76ebfdf3abd7e4b8e24a5a2e57462fce481
chronomeister <chronomeista@gmail.com>
2017-07-10 11:55:55 -0500

    I should totally put on a message for when it is released.

--- hummingbird.js
+++ hummingbird.js
@@ -8,10 +8,15 @@ exports.when = function(msg) {
     var hours = Math.floor(Math.floor(Math.floor(time / 1000) / 60) / 60) % 24;
     var minutes = Math.floor(Math.floor(time / 1000) / 60) % 60;
     var seconds = Math.floor(time / 1000) % 60;
-	var message = "The release of Hummingbird on THE IDOLM@STER MILLION LIVE! M@STER SPARKLE 02 CD is in " +
+	var message = "";
+	if (n > r) {
+		message = "THE IDOLM@STER MILLION LIVE! M@STER SPARKLE 02 CD WITH THE TRACK OF Hummingbird HAS BEEN RELEASED. TIME TO ~~CAMP HnA TO PIRATE IT~~ BUY IT!";
+	} else {
+		message = "The release of Hummingbird on THE IDOLM@STER MILLION LIVE! M@STER SPARKLE 02 CD is in " +
 		((days > 0) ? days + " day" + (days == 1 ? "" : "s") + " " : "") +
 		((hours > 0) ? hours + " hour" + (hours == 1 ? "" : "s") + " " : "") +
 		((minutes > 0) ? minutes + " minute" + (minutes == 1 ? "" : "s") + " " : "") +
 		((seconds > 0) ? seconds + " second" + (seconds == 1 ? "" : "s") + " " : "");
+	}
 	msg.channel.send(message);
 }
