f0874365d5ed045426570729df64b4fe2721ca04
chronomeister <masay87@yahoo.com>
2018-03-11 02:39:43 +0900

    start of twitter stream

--- twitter.js
+++ twitter.js
@@ -3,8 +3,22 @@ const fs = require('fs');
 var Twitter = require('twitter');
 var request = require('request');
 var client = new Twitter({
-  consumer_key: auth.twitterkey,
-  consumer_secret: auth.twittersecret,
-  bearer_token: auth.twitterbtoken
+	consumer_key: auth.twitterkey,
+	consumer_secret: auth.twittersecret,
+	access_token_key: auth.twitteratoken,
+	access_token_secret: auth.twitterasecret,
 });
 var whconfig = require('./webhookconfig.json');
+
+// load list to browse;
+// start stream listen;
+client.stream('statuses/filter', {track: 'twitter'},  function(stream) {
+	stream.on('data', function(tweet) {
+		// var util = require('util');
+		// fs.appendFile('./twitter.txt', util.inspect(tweet) + "\n");
+	});
+
+	stream.on('error', function(error) {
+		console.log(error);
+	});
+});
