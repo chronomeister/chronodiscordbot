bf260a40d0eaab37cde6dafc2445f00904b91a21
chronomeister <masay87@yahoo.com>
2018-04-07 10:05:23 +0900

    fixed that profile image (and updated tokens)

--- webhook.js
+++ webhook.js
@@ -40,9 +40,11 @@ function getStatus(first) {
 		} else {
 			if (user.screen_name == "KanColle_STAFF") {
 				client.get('users/show', {screen_name : user.screen_name}, function(error, tweets, response) {
-					if (response.body && response.body.profile_image_url_https) {
+					if (response.body) {
 						var info = JSON.parse(response.body);
-						if (kcprofileimg != info.profile_image_url_https) {
+						console.dir(info.profile_image_url_https);
+						const fs = require('fs'); var util = require('util'); fs.appendFile('./dump.txt', util.inspect(info, {depth : 9}) + "\n", () => {});
+						if (info.profile_image_url_https && kcprofileimg != info.profile_image_url_https) {
 							kcprofileimg = info.profile_image_url_https;
 							logstr(profilelogfile, false, user.screen_name, kcprofileimg);
 							var lrg = kcprofileimg.replace("_normal", "");
